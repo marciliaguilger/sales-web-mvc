7195b693537f0234e68617787cf1f8b0bffd62bb
Marcilia <guilgerm@gmail.com>
2020-04-11 11:00:37 -0300

    Delete Seller

--- SaleWebMVC/Services/SellerService.cs
+++ SaleWebMVC/Services/SellerService.cs
@@ -19,6 +19,18 @@ namespace SaleWebMVC.Services
         {
             return _context.Seller.ToList();
         }
+        
+        public Seller FindById(int id)
+        {
+            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+        }
+
+        public void Remove(int id)
+        {
+            var obj = _context.Seller.Find(id);
+            _context.Seller.Remove(obj); //removido do dbset
+            _context.SaveChanges();  //salvando as alterações no banco de dados
+        }
 
         public void Insert(Seller obj)
         {
